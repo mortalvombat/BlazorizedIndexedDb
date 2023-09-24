bd732f4c1889d3d7dcdeaa22e1ef8f6d38edb337
Karmakar-EXT Palash (uie37359) <palash.2.karmakar-EXT@conti.de>
2023-09-24 02:21:50 +0000

    refactored

--- IndexDb.Example/Pages/Index.razor.cs
+++ IndexDb.Example/Pages/Index.razor.cs
@@ -27,14 +27,14 @@ namespace IndexDb.Example.Pages
                     if (AllThePeeps.Count() < 1)
                     {
                         Person[] persons = new Person[] {
-                    new Person { Name = "Zack", TestInt = 9, _Age = 45, GUIY = Guid.NewGuid(), Secret = "I buried treasure behind my house"},
-                    new Person { Name = "Luna", TestInt = 9, _Age = 35, GUIY = Guid.NewGuid(), Secret = "Jerry is my husband and I had an affair with Bob."},
-                    new Person { Name = "Jerry", TestInt = 9, _Age = 35, GUIY = Guid.NewGuid(), Secret = "My wife is amazing"},
-                    new Person { Name = "Jon", TestInt = 9, _Age = 37, GUIY = Guid.NewGuid(), Secret = "I black mail Luna for money because I know her secret"},
-                    new Person { Name = "Jack", TestInt = 9, _Age = 37, GUIY = Guid.NewGuid(), Secret = "I have a drug problem"},
-                    new Person { Name = "Cathy", TestInt = 9, _Age = 22, GUIY = Guid.NewGuid(), Secret = "I got away with reading Bobs diary."},
-                    new Person { Name = "Bob", TestInt = 3 , _Age = 69, GUIY = Guid.NewGuid(), Secret = "I caught Cathy reading my diary, but I'm too shy to confront her." },
-                    new Person { Name = "Alex", TestInt = 3 , _Age = 80, GUIY = Guid.NewGuid(), Secret = "I'm naked! But nobody can know!" }
+                    new() { Name = "Zack", TestInt = 9, _Age = 45, GUIY = Guid.NewGuid(), Secret = "I buried treasure behind my house"},
+                    new() { Name = "Luna", TestInt = 9, _Age = 35, GUIY = Guid.NewGuid(), Secret = "Jerry is my husband and I had an affair with Bob."},
+                    new() { Name = "Jerry", TestInt = 9, _Age = 35, GUIY = Guid.NewGuid(), Secret = "My wife is amazing"},
+                    new() { Name = "Jon", TestInt = 9, _Age = 37, GUIY = Guid.NewGuid(), Secret = "I black mail Luna for money because I know her secret"},
+                    new() { Name = "Jack", TestInt = 9, _Age = 37, GUIY = Guid.NewGuid(), Secret = "I have a drug problem"},
+                    new() { Name = "Cathy", TestInt = 9, _Age = 22, GUIY = Guid.NewGuid(), Secret = "I got away with reading Bobs diary."},
+                    new() { Name = "Bob", TestInt = 3 , _Age = 69, GUIY = Guid.NewGuid(), Secret = "I caught Cathy reading my diary, but I'm too shy to confront her." },
+                    new() { Name = "Alex", TestInt = 3 , _Age = 80, GUIY = Guid.NewGuid(), Secret = "I'm naked! But nobody can know!" }
                     };
 
                         await manager.AddRange(persons);
@@ -42,9 +42,9 @@ namespace IndexDb.Example.Pages
 
 
                     //var StorageLimit = await manager.GetStorageEstimateAsync();
-                    var storageInfo = await manager.GetStorageEstimateAsync();
-                    storageQuota = storageInfo.quota;
-                    storageUsage = storageInfo.usage;
+                    var (quota, usage) = await manager.GetStorageEstimateAsync();
+                    storageQuota = quota;
+                    storageUsage = usage;
 
                     var allPeopleDecrypted = await manager.GetAll<Person>();
 
